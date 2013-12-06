cc4f9a48690cc2f0222ca74d6b087c67e35f216a
Tyler Garlick <tjgarlick@gmail.com>
2013-12-05 23:17:59 -0500

    removed packages, initial drop

---
+++ Src/FlitBit.IoC.SignalR/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hosting;
+
+namespace FlitBit.IoC.SignalR
+{
+    
+    public class FlitBitDependencyResolver : DefaultDependencyResolver, IDependencyResolver
+    {
+        public override object GetService(Type serviceType)
+        {
+            var hostContext = GlobalHost.DependencyResolver.Resolve<HostContext>();
+            if (hostContext != null)
+            {
+                
+            }
+            return base.GetService(serviceType);
+        }
+    }
+}
